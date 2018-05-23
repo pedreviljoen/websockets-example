23d30e4cad0e396d42f54ab313a08079d4042284
Pedre <pviljoen@fullfacing.com>
2018-05-23 21:46:10 +0200

    websocket starting point

---
+++ server.js
@@ -0,0 +1,24 @@
+// Simple express server and websocket example
+
+// node dependancies
+const express = require('express')
+const ws = require('ws')
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+// kickup express server
+
+const server = app.use((req, res) => res.send({'working': true})).listen(PORT, () => console.log('Listening on ' + PORT))
+const wss = new ws.Server({ server })
+
+wss.on('connection', (ws) => {
+  // add a simple event
+  ws.on('message', (message) => {
+    ws.send('Hello, you have sent ' + message)
+  })
+
+  //send immediate feedback to incoming connection
+  ws.send('Hi there I am a WebSocket')
+})
+
